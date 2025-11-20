57970d15be386be8ecb8611361ed2745952ce6c8
Diego Ortiz Castro <58611824+DiegoOC111@users.noreply.github.com>
2025-11-19 22:23:11 -0300

    Se agregan  validaciones al service
    Se agregan validaciones alosservices

--- src/main/java/com/example/LevelUp/service/TipoDespachoService.java
+++ src/main/java/com/example/LevelUp/service/TipoDespachoService.java
@@ -25,10 +25,16 @@ public class TipoDespachoService {
     }
 
     public TipoDespacho save(TipoDespacho tipoDespacho) {
+        if (tipoDespacho.getNombreDespacho() == null || tipoDespacho.getNombreDespacho().trim().isEmpty()) {
+            throw new IllegalArgumentException("El nombre del tipo de despacho no puede estar vacío");
+        }
         return tipoDespachoRepository.save(tipoDespacho);
     }
 
     public TipoDespacho update(Integer id, TipoDespacho data) {
+         if (data.getNombreDespacho() == null || data.getNombreDespacho().trim().isEmpty()) {
+            throw new IllegalArgumentException("El nombre del tipo de despacho no puede estar vacío");
+        }
         return tipoDespachoRepository.findById(id).map(td -> {
             td.setNombreDespacho(data.getNombreDespacho());
             return tipoDespachoRepository.save(td);
